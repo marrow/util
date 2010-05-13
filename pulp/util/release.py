d745c7fbb23c2dfe3044d6434cb20b736d202891
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-05-12 22:06:09 -0700

    Initial package structure and meta-data.

---
+++ pulp/util/release.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+"""Release information about Pulp."""
+
+
+name = "pulp.util"
+version = "0.0.1"
+release = "0.0.1"
+
+summary = "Commonly shared Python utility subclasses and functions."
+description = """"""
+author = "Alice Bevan-McGregor"
+email = "alice+pulp@gothcandy.com"
+url = "http://github.com/pulp/pulp.util"
+download_url = "http://cheeseshop.python.org/pypi/pulp.util/"
+copyright = "2010, Alice Bevan-McGregor"
+license = "MIT"
---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys, os
+
+try:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+
+except ImportError:
+    pass
+
+from setuptools import setup, find_packages
+
+
+if sys.version_info <= (2, 5):
+    raise SystemExit("Python 2.5 or later is required.")
+
+execfile(os.path.join("pulp", "util", "release.py"))
+
+
+
+setup(
+        name = name,
+        version = version,
+        
+        description = summary,
+        long_description = description,
+        author = author,
+        author_email = email,
+        url = url,
+        download_url = download_url,
+        license = license,
+        keywords = '',
+        
+        install_requires = [],
+        
+        test_suite = 'nose.collector',
+        tests_require = ['nose', 'coverage'],
+        
+        classifiers = [
+                "Development Status :: 1 - Planning",
+                "Environment :: Console",
+                "Intended Audience :: Developers",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+                "Programming Language :: Python",
+                "Topic :: Software Development :: Libraries :: Python Modules"
+            ],
+        
+        packages = find_packages(exclude=['tests', 'tests.*', 'docs']),
+        include_package_data = True,
+        package_data = {
+                '': ['Makefile', 'README.textile', 'LICENSE', 'distribute_setup.py'],
+                'docs': ['source/*']
+            },
+        zip_safe = True,
+        
+        namespace_packages = ['pulp'],
+    )
