237c5ef7fa8c0b70c3c135081fa6a4bfd1680f2d
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-01-28 15:40:50 -0800

    Some cleanup and added a CounterMeta attribute sorting utility.

--- marrow/util/object.py
+++ marrow/util/object.py
@@ -14,11 +14,11 @@ __all__ = ['flatten', 'NoDefault', 'load_object', 'Cache', 'LoggingFile']
 
 def flatten(x):
     """flatten(sequence) -> list
-
+    
     Returns a single, flat list which contains all elements retrieved
     from the sequence and all recursively contained sub-sequences
     (iterables).
-
+    
     Examples:
     >>> [1, 2, [3,4], (5,6)]
     [1, 2, [3, 4], (5, 6)]
@@ -59,102 +59,103 @@ def load_object(target):
 
 class Cache(dict):
     """A least-recently-used (LRU) cache.
-
+    
     Discards the least recently referenced object when full.
-
+    
     Based on Python Cookbook contributions from multiple sources:
-
+    
         * http://code.activestate.com/recipes/521871/
         * http://code.activestate.com/recipes/498110/
         * http://code.activestate.com/recipes/252524/
         * http://code.activestate.com/recipes/498245/
-
+    
     And Genshi's LRUCache:
-
+    
         http://genshi.edgewall.org/browser/trunk/genshi/util.py
-
+    
     Warning: If memory cleanup is diabled this dictionary will leak.
-
+    
     """
-
+    
     class CacheElement(object):
         def __init__(self, key, value):
             self.previous = self.next = None
             self.key, self.value = key, value
-
+        
         def __repr__(self):
             return repr(self.value)
-
+    
     def __init__(self, capacity):
         super(Cache, self).__init__()
-
+        
         self.head = self.tail = None
         self.capacity = capacity
-
+    
     def __iter__(self):
         cur = self.head
-
+        
         while cur:
             yield cur.key
             cur = cur.next
-
+    
     def __getitem__(self, key):
         element = super(Cache, self).__getitem__(key)
         self._update(element)
         return element.value
-
+    
     def __setitem__(self, key, value):
         try:
             element = super(Cache, self).__getitem__(key)
             element.value = value
             self._update(element)
-
+        
         except KeyError:
             # Item doesn't exist, create a new wrapper element.
             element = self.CacheElement(key, value)
             super(Cache, self).__setitem__(key, element)
             self._insert(element)
-
+        
         self._restrict()
-
+    
     def _insert(self, element):
         element.previous, element.next = None, self.head
-
+        
         if self.head is not None:
             self.head.previous = element
-
+        
         else:
             self.tail = element
-
+        
         self.head = element
-
+    
     def _restrict(self):
         while len(self) > self.capacity:
             # element = super(Cache, self).get(self.tail.key)
             del self[self.tail.key]
-
+            
             if self.tail != self.head:
                 self.tail = self.tail.previous
                 self.tail.next = None
-
+            
             else:
                 self.head = self.tail = None
-
+    
     def _update(self, element):
         if self.head == element:
             return
-
+        
         previous = element.previous
         previous.next = element.next
-
+        
         if element.next is not None:
             element.next.previous = previous
-
+        
         else:
             self.tail = previous
-
+        
         element.previous, element.next = None, self.head
         self.head.previous = self.head = element
+
 
 class LoggingFile(object): # pragma: no cover
     """A write-only file-like object that redirects to the standard Python logging module."""
@@ -182,3 +183,18 @@ class LoggingFile(object): # pragma: no cover
     read = next
     readline = next
     readlines = next
+
+
+class CounterMeta(type):
+    '''
+    A simple meta class which adds a ``_counter`` attribute to the instances of
+    the classes it is used on. This counter is simply incremented for each new
+    instance.
+    '''
+    counter = 0
+    
+    def __call__(self, *args, **kwargs):
+        instance = type.__call__(self, *args, **kwargs)
+        instance._counter = CounterMeta.counter
+        CounterMeta.counter += 1
+        return instance
