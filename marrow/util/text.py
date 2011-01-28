082e729e27c186b5f5bcfe3f2bbdd87b7f154078
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-01-28 15:48:54 -0800

    Added ellipsis truncation and word wrapping utilities.

--- marrow/util/text.py
+++ marrow/util/text.py
@@ -5,7 +5,7 @@
 import re
 
 
-__all__ = ['normalize']
+__all__ = ['normalize', 'ellipsis', 'wrap']
 
 
 NORMALIZE_EXPRESSION = re.compile('\W+')
@@ -21,3 +21,34 @@ def normalize(name, collection=[]):
         suffix += 1
     
     return ("%s%s" % (base.strip('-'), ("-%d" % (suffix, )) if suffix else ""))
+
+
+def ellipsis(text, length, symbol="..."):
+    """Present a block of text of given length.
+    
+    If the length of available text exceeds the requested length, truncate and
+    intelligently append an ellipsis.
+    """
+    if len(text) > length:
+        pos = text.rfind(" ", 0, length)
+        if pos < 0:
+            return text[:length].rstrip(".") + symbol
+        else:
+            return text[:pos].rstrip(".") + symbol
+    
+    else:
+        return text
+
+
+def wrap(text, columns=78):
+    from textwrap import wrap
+    
+    lines = []
+    for iline in text.splitlines():
+        if not iline:
+            lines.append(iline)
+        else:
+            for oline in wrap(iline, columns):
+                lines.append(oline)
+    
+    return "\n".join(lines)
