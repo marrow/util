6091a59d35b2f9c3c50fb2b08d0bf63071269b34
Alice Bevan-McGregor <alice@gothcandy.com>
2011-08-19 19:24:10 -0400

    Unification of release information, setup script, and setup configuration.

--- setup.py
+++ setup.py
@@ -1,52 +1,34 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import sys, os
-
-try:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-
-except ImportError:
-    pass
+import os
+import sys
 
 from setuptools import setup, find_packages
 
 
-if sys.version_info <= (2, 6):
+if sys.version_info < (2, 6):
     raise SystemExit("Python 2.6 or later is required.")
 
-if sys.version_info >= (3,0):
-    def execfile(filename, globals_=None, locals_=None):
-        if globals_ is None:
-            globals_ = globals()
-        
-        if locals_ is None:
-            locals_ = globals_
-        
-        exec(compile(open(filename).read(), filename, 'exec'), globals_, locals_)
-
-else:
-    from __builtin__ import execfile
-
-execfile(os.path.join("marrow", "util", "release.py"), globals(), locals())
+exec(open(os.path.join("marrow", "util", "release.py")))
 
 
 
 setup(
-        name = name,
+        name = "marrow.util",
         version = version,
         
-        description = summary,
-        long_description = description,
-        author = author,
-        author_email = email,
-        url = url,
-        download_url = download_url,
-        license = license,
-        keywords = '',
+        description = "A collection of many commonly reimplemented utility classes and functions.",
+        long_description = """\
+For full documentation, see the README.textile file present in the package,
+or view it online on the GitHub project page:
+
+https://github.com/marrow/marrow.util""",
         
-        install_requires = [],
+        author = "Alice Bevan-McGregor",
+        author_email = "alice+marrow@gothcandy.com",
+        url = "https://github.com/marrow/marrow.util",
+        license = "MIT",
         
         test_suite = 'nose.collector',
         tests_require = ['nose', 'coverage'],
@@ -67,13 +49,10 @@ setup(
                 "Topic :: Utilities"
             ],
         
-        packages = find_packages(exclude=['tests', 'tests.*', 'docs']),
-        include_package_data = True,
-        package_data = {
-                '': ['Makefile', 'README.textile', 'LICENSE', 'distribute_setup.py'],
-                'docs': ['source/*']
-            },
+        packages = find_packages(exclude=['examples', 'tests']),
         zip_safe = True,
+        include_package_data = True,
+        package_data = {'': ['README.textile', 'LICENSE']},
         
-        namespace_packages = ['marrow', 'marrow.util'],
+        namespace_packages = ['marrow', 'marrow.util']
     )
