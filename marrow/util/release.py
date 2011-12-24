6570a66b575b5b12da484898115f2213603ec6bb
Alice Bevan-McGregor <alice@gothcandy.com>
2011-12-23 20:44:05 -0500

    Version bump.

--- marrow/util/release.py
+++ marrow/util/release.py
@@ -8,6 +8,6 @@ from collections import namedtuple
 __all__ = ['version_info', 'version']
 
 
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(1, 2, 1, 'final', 0)
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(1, 2, 2, 'final', 0)
 
 version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
