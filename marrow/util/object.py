5b7d378705381e3264c3e1ffa93e973998764266
Alice Bevan-McGregor <alice@gothcandy.com>
2013-01-27 13:50:59 -0500

    Additional support for plugin namespaces and plugin cache.

--- marrow/util/object.py
+++ marrow/util/object.py
@@ -4,7 +4,9 @@
 
 import logging
 import inspect
+import pkg_resources
 
+from collections import defaultdict
 from functools import partial
 
 from marrow.util.compat import binary, unicode
@@ -15,18 +17,18 @@ __all__ = ['flatten', 'yield_property', 'yield_keyvalue', 'NoDefault', 'load_obj
 
 def flatten(x):
     """flatten(sequence) -> list
-    
+
     Returns a single, flat list which contains all elements retrieved
     from the sequence and all recursively contained sub-sequences
     (iterables).
-    
+
     Examples:
     >>> [1, 2, [3,4], (5,6)]
     [1, 2, [3, 4], (5, 6)]
     >>> flatten([[[1,2,3], (42,None)], [4,5], [6], 7, MyVector(8,9,10)])
     [1, 2, 3, 42, None, 4, 5, 6, 7, 8, 9, 10]
     """
-    
+
     for el in x:
         if hasattr(el, "__iter__") and not isinstance(el, (binary, unicode)):
             for els in flatten(el):
@@ -51,160 +53,185 @@ NoDefault = _NoDefault()
 
 def merge(s, t):
     """Merge dictionary t into s."""
-    
+
     for k, v in t.items():
         if isinstance(v, dict):
             if k not in s:
                 s[k] = v
                 continue
-            
+
             s[k] = merge(s[k], v)
             continue
-        
+
         s[k] = v
-    
+
     return s
 
 
-def load_object(target):
+def load_object(target, namespace=None):
     """This helper function loads an object identified by a dotted-notation string.
-    
+
     For example:
-    
+
         # Load class Foo from example.objects
         load_object('example.objects:Foo')
+
+    If a plugin namespace is provided simple name references are allowed.  For example:
+
+        # Load the plugin named 'routing' from the 'web.dispatch' namespace
+        load_object('routing', 'web.dispatch')
+
+    Providing a namespace does not prevent full object lookup (dot-colon notation) from working.
     """
+    if namespace and ':' not in target:
+        allowable = dict((i.name,  i) for i in pkg_resources.iter_entry_points(namespace))
+        if target not in allowable:
+            raise ValueError('Unknown plugin "' + target + '"; found: ' + ', '.join(allowable))
+        return allowable[target].load()
+
     parts, target = target.split(':') if ':' in target else (target, None)
     module = __import__(parts)
-    
+
     for part in parts.split('.')[1:] + ([target] if target else []):
         module = getattr(module, part)
-    
+
     return module
+
+
+class PluginCache(defaultdict):
+    """Lazily load plugins from the given namespace."""
+
+    def __init__(self,  namespace):
+        super(PluginCache, self).__init__()
+        self.namespace =  namespace
+
+    def __missing__(self,  key):
+        return load_object(key,  self.namespace)
+
 
 
 class Cache(dict):
     """A least-recently-used (LRU) cache.
-    
+
     Discards the least recently referenced object when full.
-    
+
     Based on Python Cookbook contributions from multiple sources:
-    
+
         * http://code.activestate.com/recipes/521871/
         * http://code.activestate.com/recipes/498110/
         * http://code.activestate.com/recipes/252524/
         * http://code.activestate.com/recipes/498245/
-    
+
     And Genshi's LRUCache:
-    
+
         http://genshi.edgewall.org/browser/trunk/genshi/util.py
-    
+
     Warning: If memory cleanup is diabled this dictionary will leak.
-    
+
     """
-    
+
     class CacheElement(object):
         def __init__(self, key, value):
             self.previous = self.next = None
             self.key, self.value = key, value
-        
+
         def __repr__(self):
             return repr(self.value)
-    
+
     def __init__(self, capacity):
         super(Cache, self).__init__()
-        
+
         self.head = self.tail = None
         self.capacity = capacity
-    
+
     def __iter__(self):
         cur = self.head
-        
+
         while cur:
             yield cur.key
             cur = cur.next
-    
+
     def __getitem__(self, key):
         element = super(Cache, self).__getitem__(key)
         self._update(element)
         return element.value
-    
+
     def __setitem__(self, key, value):
         try:
             element = super(Cache, self).__getitem__(key)
             element.value = value
             self._update(element)
-        
+
         except KeyError:
             # Item doesn't exist, create a new wrapper element.
             element = self.CacheElement(key, value)
             super(Cache, self).__setitem__(key, element)
             self._insert(element)
-        
+
         self._restrict()
-    
+
     def _insert(self, element):
         element.previous, element.next = None, self.head
-        
+
         if self.head is not None:
             self.head.previous = element
-        
+
         else:
             self.tail = element
-        
+
         self.head = element
-    
+
     def _restrict(self):
         while len(self) > self.capacity:
             # element = super(Cache, self).get(self.tail.key)
             del self[self.tail.key]
-            
+
             if self.tail != self.head:
                 self.tail = self.tail.previous
                 self.tail.next = None
-            
+
             else:
                 self.head = self.tail = None
-    
+
     def _update(self, element):
         if self.head == element:
             return
-        
+
         previous = element.previous
         previous.next = element.next
-        
+
         if element.next is not None:
             element.next.previous = previous
-        
+
         else:
             self.tail = previous
-        
+
         element.previous, element.next = None, self.head
         self.head.previous = self.head = element
 
 
 class LoggingFile(object):
     """A write-only file-like object that redirects to the standard Python logging module."""
-    
+
     def __init__(self, logger=None, level=logging.ERROR):
         logger = logger if logger else logging.getLogger('logfile')
         self.logger = partial(logger.log, level)
-    
+
     def write(self, text):
         self.logger(text)
-    
+
     def writelines(self, lines):
         for line in lines:
             self.logger(line)
-    
-    def close(self, *args, **kw): 
+
+    def close(self, *args, **kw):
         """A no-op method used for several of the file-like object methods."""
         pass
-    
+
     def next(self, *args, **kw):
         """An error-raising exception usedbfor several of the methods."""
         raise IOError("Logging files can not be read.")
-    
+
     flush = close
     read = next
     readline = next
@@ -218,7 +245,7 @@ class CounterMeta(type):
     instance.
     '''
     counter = 0
-    
+
     def __call__(self, *args, **kwargs):
         instance = type.__call__(self, *args, **kwargs)
         instance._counter = CounterMeta.counter
@@ -228,47 +255,47 @@ class CounterMeta(type):
 
 def getargspec(obj):
     """An improved inspect.getargspec.
-    
+
     Has a slightly different return value from the default getargspec.
-    
+
     Returns a tuple of:
         required, optional, args, kwargs
         list, dict, bool, bool
-    
+
     Required is a list of required named arguments.
     Optional is a dictionary mapping optional arguments to defaults.
     Args and kwargs are True for the respective unlimited argument type.
     """
-    
+
     argnames, varargs, varkw, _defaults = None, None, None, None
-    
+
     if inspect.isfunction(obj) or inspect.ismethod(obj):
         argnames, varargs, varkw, _defaults = inspect.getargspec(obj)
-    
+
     elif inspect.isclass(obj):
         if inspect.ismethoddescriptor(obj.__init__):
             argnames, varargs, varkw, _defaults = [], False, False, None
-        
+
         else:
             argnames, varargs, varkw, _defaults = inspect.getargspec(obj.__init__)
-    
+
     elif hasattr(obj, '__call__'):
         argnames, varargs, varkw, _defaults = inspect.getargspec(obj.__call__)
-    
+
     else:
         raise TypeError("Object not callable?")
-    
+
     # Need test case to prove this is even possible.
     # if (argnames, varargs, varkw, defaults) is (None, None, None, None):
     #     raise InspectionFailed()
-    
+
     if argnames and argnames[0] == 'self':
         del argnames[0]
-    
+
     if _defaults is None:
         _defaults = []
         defaults = dict()
-    
+
     else:
         # Create a mapping dictionary of defaults; this is slightly more useful.
         defaults = dict()
@@ -277,8 +304,8 @@ def getargspec(obj):
         argnames.reverse()
         for i, default in enumerate(_defaults):
             defaults[argnames[i]] = default
-    
+
         argnames.reverse()
         # del argnames[-len(_defaults):]
-    
+
     return argnames, defaults, True if varargs else False, True if varkw else False
