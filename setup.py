b5d97b5a9e6913c7a3795a8e34b9bfe2704e1f1b
Alice Zoë Bevan–McGregor <amcgregor@cegid.com>
2022-12-01 10:56:19 -0500

    Namespace packaging changes.

--- setup.py
+++ setup.py
@@ -4,55 +4,55 @@
 import os
 import sys
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 if sys.version_info < (2, 6):
-    raise SystemExit("Python 2.6 or later is required.")
+	raise SystemExit("Python 2.6 or later is required.")
 
 exec(open(os.path.join("marrow", "util", "release.py")).read())
 
 
 
 setup(
-        name = "marrow.util",
-        version = version,
-        
-        description = "A collection of many commonly reimplemented utility classes and functions.",
-        long_description = """\
+		name = "marrow.util",
+		version = version,
+		
+		description = "A collection of many commonly reimplemented utility classes and functions.",
+		long_description = """\
 For full documentation, see the README.textile file present in the package,
 or view it online on the GitHub project page:
 
 https://github.com/marrow/marrow.util""",
-        
-        author = "Alice Bevan-McGregor",
-        author_email = "alice+marrow@gothcandy.com",
-        url = "https://github.com/marrow/marrow.util",
-        license = "MIT",
-        
-        test_suite = 'nose.collector',
-        tests_require = ['nose', 'coverage'],
-        
-        classifiers = [
-                "Development Status :: 5 - Production/Stable",
-                "Environment :: Console",
-                "Intended Audience :: Developers",
-                "License :: OSI Approved :: MIT License",
-                "Operating System :: OS Independent",
-                "Programming Language :: Python",
-                "Programming Language :: Python :: 2.6",
-                "Programming Language :: Python :: 2.7",
-                "Programming Language :: Python :: 3",
-                "Programming Language :: Python :: 3.1",
-                "Programming Language :: Python :: 3.2",
-                "Topic :: Software Development :: Libraries :: Python Modules",
-                "Topic :: Utilities"
-            ],
-        
-        packages = find_packages(exclude=['examples', 'tests']),
-        zip_safe = True,
-        include_package_data = True,
-        package_data = {'': ['README.textile', 'LICENSE']},
-        
-        namespace_packages = ['marrow', 'marrow.util']
-    )
+		
+		author = "Alice Bevan-McGregor",
+		author_email = "alice+marrow@gothcandy.com",
+		url = "https://github.com/marrow/marrow.util",
+		license = "MIT",
+		
+		test_suite = 'nose.collector',
+		tests_require = ['nose', 'coverage'],
+		
+		classifiers = [
+				"Development Status :: 5 - Production/Stable",
+				"Environment :: Console",
+				"Intended Audience :: Developers",
+				"License :: OSI Approved :: MIT License",
+				"Operating System :: OS Independent",
+				"Programming Language :: Python",
+				"Programming Language :: Python :: 2.6",
+				"Programming Language :: Python :: 2.7",
+				"Programming Language :: Python :: 3",
+				"Programming Language :: Python :: 3.1",
+				"Programming Language :: Python :: 3.2",
+				"Topic :: Software Development :: Libraries :: Python Modules",
+				"Topic :: Utilities"
+			],
+		
+		packages = ('marrow.util', ),
+		zip_safe = True,
+		include_package_data = True,
+		package_data = {'': ['README.textile', 'LICENSE']},
+		
+		namespace_packages = ['marrow', 'marrow.util']
+	)
